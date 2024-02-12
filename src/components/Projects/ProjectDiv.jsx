008479fa342da011b648dd77794f008d6c1a1225
Larry Reaux <larryreaux@gmail.com>
2024-02-11 18:05:40 -0600

    changed from map to find

--- src/components/Projects/ProjectDiv.jsx
+++ src/components/Projects/ProjectDiv.jsx
@@ -1,9 +1,5 @@
 export default function ProjectDiv({ cardArr, count }) {
   return (
-    <div className="project-div">
-      {cardArr.map((card, i) => {
-        if (count == i) return card;
-      })}
-    </div>
+    <div className="project-div">{cardArr.find((card, i) => count == i)}</div>
   );
 }
